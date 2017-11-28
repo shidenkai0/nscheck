ce2f31c74d6cc85838b6bc4cbea2c918bacecd54
Mohamed Messaad <mohamed.messaad@classnco.com>
2017-11-28 16:10:23 +0100

    implement ns streaming from csv

--- main.go
+++ main.go
@@ -3,24 +3,22 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 
 	"github.com/shidenkai0/nscheck/ns"
 )
 
 func main() {
-	domain := flag.String("d", "google.com", "domain to query")
+	name := flag.String("n", "google.com", "DNS name to query")
 	rType := flag.String("t", "A", "Record Type to query")
 	serversCSV := flag.String("f", "nameservers-all.csv", "DNS Servers list (CSV)")
-	nsList, err := ns.Load(*serversCSV)
-	if err != nil {
-		log.Fatalln(fmt.Errorf("Loading NS List: %v", err))
+	flag.Parse()
+
+	queryLogsChan := ns.PerformFromCSV(ns.Query{RecordType: *rType, Name: *name}, *serversCSV)
+	var successCount int
+	for ql := range queryLogsChan {
+		if ql.Err == nil {
+			successCount++
+		}
 	}
-	queryLogs, err := nsList.Query(*domain, *rType)
-	if err != nil {
-		panic(err)
-	}
-	if len(queryLogs) >= 1 { // Print only first NSLog so far
-		fmt.Println(queryLogs[0])
-	}
+	fmt.Println("Successful queries:", successCount)
 }
--- ns/ns.go
+++ ns/ns.go
@@ -4,11 +4,17 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/gocarina/gocsv"
 	"github.com/miekg/dns"
 )
+
+type Query struct {
+	RecordType string
+	Name       string
+}
 
 // NS represents a name server
 type NS struct {
@@ -22,9 +28,11 @@ type NS struct {
 }
 
 // Query a name server
-func (ns *NS) Query(domain, recordType string) ([]string, error) {
+func (ns *NS) Perform(q Query) ([]string, error) {
+	q.Name += "."
 	m := new(dns.Msg)
-	m.SetQuestion(domain, dns.StringToType[recordType])
+	m.SetQuestion(q.Name, dns.StringToType[q.RecordType])
+	//c := new(dns.Client)
 	res, err := dns.Exchange(m, fmt.Sprintf("%v:53", ns.IP))
 	if err != nil {
 		return nil, err
@@ -36,35 +44,95 @@ func (ns *NS) Query(domain, recordType string) ([]string, error) {
 	return answers, nil
 }
 
+type NSQuery struct {
+	Ns NS
+	Q  Query
+}
+
+func (nsq *NSQuery) perform() QueryResult {
+	answers, err := nsq.Ns.Perform(nsq.Q)
+	return QueryResult{Server: nsq.Ns, Answers: answers, Q: nsq.Q, Err: err}
+}
+
 // NSList is a queriable list of NSes
 type NSList []NS
 
-func (nsl *NSList) Query(domain, recordType string) ([]NSLog, error) {
-	if nsl == nil {
-		return nil, fmt.Errorf("ns: Query on nil NSList")
-	}
-	var qls []NSLog // query logs
-	for _, ns := range *nsl {
-		answers, err := ns.Query(domain, recordType)
-		ql := NSLog{
-			Server:  ns,
-			Err:     err,
-			Answers: answers,
-		}
-		qls = append(qls, ql)
-	}
-	return qls, nil
+func (nsl *NSList) Perform(q Query) <-chan QueryResult {
+	return process(nsl.sequentialQueries(q), 256)
 }
 
-// NSLog represents a set of DNS answers along with metadata on the associated name server
-type NSLog struct {
+func (nsl *NSList) sequentialQueries(q Query) <-chan NSQuery {
+	out := make(chan NSQuery)
+	go func() {
+		for _, ns := range *nsl {
+			out <- NSQuery{Ns: ns, Q: q}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func process(in <-chan NSQuery, concurrency int) <-chan QueryResult {
+	out := make(chan QueryResult)
+	wg := sync.WaitGroup{}
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+		go func() {
+			for nsq := range in {
+				ql := nsq.perform()
+				out <- ql
+			}
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+// QueryResult represents a set of DNS answers along with metadata on the associated name server
+type QueryResult struct {
 	Server  NS
+	Q       Query
 	Err     error
 	Answers []string
 }
 
-// Load loads a DNS list from CSV
-func Load(fileName string) (NSList, error) {
+func buildNSQueries(in <-chan NS, q Query) <-chan NSQuery {
+	out := make(chan NSQuery)
+	go func() {
+		for ns := range in {
+			out <- NSQuery{Q: q, Ns: ns}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func StreamFromCSV(fileName string) <-chan NS {
+	out := make(chan NS)
+	nsfile, err := os.Open(fileName)
+	if err != nil {
+		return nil
+	}
+
+	go func() {
+		if err := gocsv.UnmarshalToChan(nsfile, out); err != nil {
+			fmt.Println(err)
+		}
+		nsfile.Close()
+	}()
+	return out
+}
+
+func PerformFromCSV(q Query, fileName string) <-chan QueryResult {
+	return process(buildNSQueries(StreamFromCSV(fileName), q), 256)
+}
+
+// LoadFromCSV loads a DNS list from CSV
+func LoadFromCSV(fileName string) (NSList, error) {
 	nsfile, err := os.Open(fileName)
 	if err != nil {
 		return nil, fmt.Errorf("ns.Load: %v", err)
